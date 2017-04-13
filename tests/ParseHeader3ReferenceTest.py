71d845d4eaa1eca9bab6bec659fc7cf7802e47d4
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2017-04-13 18:01:45 +0200

    Add header3 reference unit tests.

---
+++ tests/ParseHeader3ReferenceTest.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+from DuralexTestCase import DuralexTestCase
+
+import duralex.alinea_parser as parser
+
+class ParseHeader3ReferenceTest(DuralexTestCase):
+    def test_header3(self):
+        self.assertEqualAST(
+            self.call_parse_func(
+                parser.parse_header3_reference,
+                "au e"
+            ),
+            {'children':[
+                {
+                    'type': u'header3-reference',
+                    'order': 5
+                }
+            ]}
+        )
+
+    def test_before_header3(self):
+        self.assertEqualAST(
+            self.call_parse_func(
+                parser.parse_header3_reference,
+                "avant le d"
+            ),
+            {'children':[
+                {
+                    'type': u'header3-reference',
+                    'position': u'before',
+                    'order': 4
+                }
+            ]}
+        )
+
+    def test_header3_header2_article_code(self):
+        self.assertEqualAST(
+            self.call_parse_func(
+                parser.parse_header3_reference,
+                "le b du 3° de l'article L. 711-2 du code de l'éducation"
+            ),
+            {'children': [
+                {
+                    'order': 2,
+                    'type': u'header3-reference',
+                    'children': [
+                        {
+                            'order': 3,
+                            'type': u'header2-reference',
+                            'children': [
+                                {
+                                    'children': [
+                                        {
+                                            'codeName': u'code de l\'éducation',
+                                            'type': u'code-reference'
+                                        }
+                                    ],
+                                    'id': u'L. 711-2',
+                                    'type': u'article-reference'
+                                }
+                            ],
+                        }
+                    ]
+                }
+            ]}
+        )
