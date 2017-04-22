01730d11c662a894191470e34025ea1b8f249d79
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2017-04-22 10:41:09 +0200

    Declare lib dependencies in setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='DuraLex',
+    version='0.1',
+    install_requires=[
+        'html5lib',
+        'simplejson',
+        'beautifulsoup4'
+    ]
+)
