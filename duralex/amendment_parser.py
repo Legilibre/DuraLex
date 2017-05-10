e781a03c067c4976b5b1faba7414a2e30d8a66eb
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2017-05-10 07:22:43 +0200

    Add the "number" field on "amendment" nodes.

--- duralex/amendment_parser.py
+++ duralex/amendment_parser.py
@@ -28,6 +28,7 @@ def parse_amendment(data, parent):
     tokens = lexer.tokenize(subject + '\n' + text)
     node = create_node(parent, {
         'type': 'amendment',
+        'number': data['numero'],
         'content': text,
         'status': AMENDMENT_STATUS[data['sort'].lower()],
         'description': clean_html(data['expose']),
