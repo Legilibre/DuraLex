19a386d5ac20e5d82d25f6ff1d20134ef8e53683
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2017-05-10 07:22:03 +0200

    Fix the missing article-reference parsed from the admendment's "subject".

--- duralex/amendment_parser.py
+++ duralex/amendment_parser.py
@@ -21,22 +21,37 @@ def parse(data, tree):
         amendements.append(parse_amendment(amendement['amendement'], tree))
     return tree
 
-import re
-
-def parse_amendment(data, node):
-    subject = data['amendement']['sujet']
-    text = clean_html(data['amendement']['texte'])
+def parse_amendment(data, parent):
+    subject = data['sujet']
+    text = clean_html(data['texte'])
 
     tokens = lexer.tokenize(subject + '\n' + text)
-    node = create_node(node, {
+    node = create_node(parent, {
         'type': 'amendment',
         'content': text,
-        'status': AMENDMENT_STATUS[data['amendement']['sort'].lower()],
-        'description': clean_html(data['amendement']['expose']),
-        'signatories': [s.strip() for s in data['amendement']['signataires'].split(', ')]
+        'status': AMENDMENT_STATUS[data['sort'].lower()],
+        'description': clean_html(data['expose']),
+        'signatories': [s.strip() for s in data['signataires'].split(', ')]
     })
 
-    parse_subject(tokens, 0, node)
+    # The "subject" declares the article referenced by the admendment as a whole.
+    # We create a "fake" 'edit' node with the corresponding 'article-reference' node
+    # and the ResolveFullyQualifiedReferencesVisitor will take care of properly transforming this:
+    #
+    # - amendment
+    #   - edit <- our "fake" 'edit' node
+    #     - article-reference <- the 'article-reference' node coming from the "subject"
+    #   - edit <- the actual 'edit' node coming from the admendment text
+    #     - *-reference
+    #
+    # into this:
+    #
+    # - amendment
+    #   - edit <- the actual 'edit' node coming from the admendment text
+    #     - article-reference <- the 'article-reference' node coming from the "subject"
+    #       - *-reference
+    fake_edit = create_node(node, {'type': 'edit', 'editType': 'edit'})
+    parse_subject(tokens, 0, fake_edit)
     parse_alineas(node['content'], node)
 
     return node
@@ -50,11 +65,11 @@ def parse_subject(tokens, i, parent):
 
     # ART. PREMIER
     if tokens[i] == 'ART' and is_number_word(tokens[i + 3]):
-        node['id'] = word_to_number(tokens[i + 3])
+        node['id'] = str(word_to_number(tokens[i + 3]))
         i += lexer.skip_to_end_of_line(tokens, i)
     # ART. {id}
     elif tokens[i] == 'ART' and is_number(tokens[i + 3]):
-        node['id'] = parse_int(tokens[i + 3])
+        node['id'] = tokens[i + 3]
         i += lexer.skip_to_end_of_line(tokens, i)
 
     return i
