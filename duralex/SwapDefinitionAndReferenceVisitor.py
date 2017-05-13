e6650ecf81c47728a0f61dd2549950c34ee59146
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2017-05-13 22:33:51 +0200

    Add the SwapDefinitionAndReferenceVisitor.

---
+++ duralex/SwapDefinitionAndReferenceVisitor.py
@@ -0,0 +1,11 @@
+from AbstractVisitor import AbstractVisitor
+
+import duralex.tree as tree
+
+class SwapDefinitionAndReferenceVisitor(AbstractVisitor):
+    def visit_edit_node(self, node, post):
+        defs = filter(lambda n: tree.is_definition(n), node['children'])
+
+        for d in defs:
+            tree.remove_node(node, d)
+            tree.push_node(node, d)
