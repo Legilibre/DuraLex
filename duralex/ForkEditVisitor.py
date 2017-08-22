Legilibre/DuraLex

commit 35141e7f61ef7c6c372f0044eebbd630472277f8

replace list(filter(..)) with list comprehension

duralex/ForkEditVisitor.py
@@ -7,8 +7,8 @@ import duralex.tree
 class ForkEditVisitor(AbstractVisitor):
     def visit_node(self, node):
         if 'type' in node and node['type'] == 'edit' and 'children' in node and len(node['children']) > 1:
-            ref_nodes = list(filter(lambda n: duralex.tree.is_reference(n), node['children']))
-            def_nodes = list(filter(lambda n: duralex.tree.is_definition(n), node['children']))
+            ref_nodes = [n for n in node['children' if duralex.tree.is_reference(n)]
+            def_nodes = [n for n in node['children' if duralex.tree.is_definition(n)]
             edit_node = copy_node(node, recursive=False)
             parent = node['parent']
             remove_node(parent, node)

duralex/FixMissingCodeOrLawReferenceVisitor.py
@@ -27,10 +27,9 @@ class FixMissingCodeOrLawReferenceVisitor(AbstractVisitor):
     def visit_article_reference_node(self, node, post):
         if post:
             return
-        ancestor_refs = list(filter(
-            lambda n: not is_root(n) and n['type'] in [TYPE_CODE_REFERENCE, TYPE_LAW_REFERENCE],
-            get_node_ancestors(node) + get_node_descendants(node)
-        ))
+        ancestor_refs = [n for n in get_node_ancestors(node) + get_node_descendants(node) if
+            not is_root(n) and n['type'] in [TYPE_CODE_REFERENCE, TYPE_LAW_REFERENCE]
+        ]
         if len(ancestor_refs) == 0:
             while len(node['children']) != 0:
                 node = node['children'][0]

duralex/ForkReferenceVisitor.py
@@ -7,7 +7,7 @@ import duralex.tree
 class ForkReferenceVisitor(AbstractVisitor):
     def visit_node(self, node):
         if duralex.tree.is_reference(node) and 'children' in node and len(node['children']) > 1:
-            ref_nodes = list(filter(lambda n: duralex.tree.is_reference(n), node['children']))
+            ref_nodes = [n for n in node['children'] if duralex.tree.is_reference(n)]
             for i in range(1, len(ref_nodes)):
                 ref = ref_nodes[i]
                 fork = copy_node(node, recursive=False)

duralex/alinea_lexer.py
@@ -58,7 +58,7 @@ def tokenize(text):
 
     tokens = TOKEN_DELIMITERS.split(text)
     # remove empty strings
-    tokens = list(filter(lambda s: s != '', tokens))
+    tokens = [s for s in tokens if s != '']
     return tokens
 
 def skip_tokens(tokens, i, f):

commit 042f2a4ff38cc68ba4fe310c54854ecd116704e4

fix typo introduced in 35141e7f61ef7c6c372f0044eebbd630472277f8

duralex/ForkEditVisitor.py
@@ -7,8 +7,8 @@ import duralex.tree
 class ForkEditVisitor(AbstractVisitor):
     def visit_node(self, node):
         if 'type' in node and node['type'] == 'edit' and 'children' in node and len(node['children']) > 1:
-            ref_nodes = [n for n in node['children' if duralex.tree.is_reference(n)]
-            def_nodes = [n for n in node['children' if duralex.tree.is_definition(n)]
+            ref_nodes = [n for n in node['children'] if duralex.tree.is_reference(n)]
+            def_nodes = [n for n in node['children'] if duralex.tree.is_definition(n)]
             edit_node = copy_node(node, recursive=False)
             parent = node['parent']
             remove_node(parent, node)