92fbf483b15dda2e0e30b44f5917dc1d8ef0bc3e
Seb35 <seb35@seb35.fr>
2018-12-23 16:07:34 +0100

    Add more checks in RemoveQuotePrefixVisitor
    In particular, check than the removed prefix "Art. {articleId}. -"
    is the same than its container article-definition (if this one declared
    the article name) and check that the dash in the articleId is the standard
    dash (possibly the article-definition nodes could force their id to be a
    standard dash).
    
    Also the very last dash in "Art. {articleId}. -" can be any type of
    dash between the 7 dashed defined in Unicode.
    
    Also removed the multiline flag, because if there are multiple articles
    defined in a single 'word' node, they should be splitted into independant
    'article-definition'+'word' nodes (see #10).

--- duralex/RemoveQuotePrefixVisitor.py
+++ duralex/RemoveQuotePrefixVisitor.py
@@ -5,9 +5,23 @@ from duralex.AbstractVisitor import AbstractVisitor
 from duralex.alinea_parser import *
 
 class RemoveQuotePrefixVisitor(AbstractVisitor):
+    __slots__ = ['articleId']
+
+    def visit_article_definition_node(self, node, post):
+        self.articleId = None
+        if not post and 'id' in node:
+            self.articleId = node['id']
+
     def visit_quote_node(self, node, post):
         if post:
             return
 
         # Art. {articleId}. -
-        node['words'] = re.sub(r'^Art\. .*?\. - ', '', node['words'], 0, re.UNICODE | re.MULTILINE)
+        s = re.match(r'^Art\. (.*?)\. +[-‐‑‒–—―] +', node['words'])
+        if s == None or self.articleId == None:
+            return
+
+        if self.articleId == re.sub(r'[-‐‑‒–—―]', '-', s.group(1)):
+            node['words'] = re.sub(r'^Art\. .*?\. +[-‐‑–—] +', '', node['words'])
+        else:
+            raise Exception('Prefix "Art. {articleId}. - " in text different than previously declared article n°{articleId}, sounds strange')
